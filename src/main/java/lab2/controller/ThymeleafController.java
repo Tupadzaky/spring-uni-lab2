886d81d68330ac71c91f5732af67448384aeeff8
Valeriia <lera22062004@gmail.com>
2023-11-30 23:17:40 +0200

    feat(spring-uni-lab3): changed Thymeleaf

--- src/main/java/lab2/controller/ThymeleafController.java
+++ src/main/java/lab2/controller/ThymeleafController.java
@@ -4,21 +4,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ThymeleafController {
 
     @RequestMapping("/index")
     ModelAndView getPibOfCreators() {
         ModelAndView index = new ModelAndView("index");
-        index.addObject("student1", "Кохан Валерія");
-        index.addObject("student2", "Хижнях Валерія");
-        index.addObject("student3", "Крутиус Владислав");
-        index.addObject("student4", "Запотоцький Іван");
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Кохан Валерія"));
+        students.add(new Student(2, "Хижнях Валерія"));
+        students.add(new Student(3, "Крутиус Владислав"));
+        students.add(new Student(4, "Запотоцький Іван"));
+        index.addObject("students", students);
         return index;
     }
 
-    @RequestMapping("/")
-    String thymeleafView() {
-        return "index";
+    class Student {
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        int id;
+        String name;
+
+        public Student(int i, String name) {
+            this.id = i;
+            this.name = name;
+        }
     }
 }
